f6ae0ef6db140da8ae43acb44af3825e8412a1aa
ys13tp-er <907861049@qq.com>
2019-09-02 17:25:41 +0800

    build文件

---
+++ build/precache-manifest.8a6472b2646861b09377f4de8a904dac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf4a7a92cc0070be075f83fbf250067",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b5f426df93c69e453777",
+    "url": "./static/css/2.a1b8edf3.chunk.css"
+  },
+  {
+    "revision": "02a7880da97c6dc36796",
+    "url": "./static/css/main.e4404fb6.chunk.css"
+  },
+  {
+    "revision": "b5f426df93c69e453777",
+    "url": "./static/js/2.f28e3e1d.chunk.js"
+  },
+  {
+    "revision": "02a7880da97c6dc36796",
+    "url": "./static/js/main.2ec84746.chunk.js"
+  },
+  {
+    "revision": "071ba782ee03d8d57f39",
+    "url": "./static/js/runtime~main.aa7fb552.js"
+  },
+  {
+    "revision": "9f4eb1df05a902c1e4282153fb806ce2",
+    "url": "./static/media/logo.9f4eb1df.png"
+  }
+]);
